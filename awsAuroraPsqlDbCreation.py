6bc6f7ed76975477ab4656343c8ee7a947cc127e
bourim@wni.com <ahlouma1950>
2018-09-12 08:32:20 +0200

    no test

---
+++ awsAuroraPsqlDbCreation.py
@@ -0,0 +1,27 @@
+__author__="tb"
+from decide2_tests.common.system.ServiceLoader import ServiceLoader
+
+
+
+class aurora_psql:
+
+    """
+        AWS credentials must be set up under /home/"user"/.aws/credentiels
+        and /home/"user"/.aws/config
+        see : https://boto3.readthedocs.io/en/latest/guide/quickstart.html
+
+    """
+    def __init__(self):
+        self.dao = ServiceLoader.load_dao("DecidePostgresDao")
+
+
+
+
+if __name__ == '__main__':
+
+    db = aurora_psql()
+    db.dao.insert_points()
+
+
+
+
